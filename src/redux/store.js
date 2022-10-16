75c0c922a985334bb31bcc9ad79e79998d431726
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-10-16 18:08:55 +0300

    start

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+
+const toAdd = createAction('myValue/toAdd');
+const toDelete = createAction('myValue/toDelete');
+
+const myReducer = createReducer(10, {
+  [toAdd]: (state, action) => state + action.payload,
+  [toDelete]: (state, action) => state - action.payload,
+});
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    myValue: myReducer,
+  },
 });
